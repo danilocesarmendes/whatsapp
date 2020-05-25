0aefad7c3570479e49b6c417fd52eabcc43971a7
Danilo Cesar Mendes <danilocesarmendes@gmail.com>
2020-05-25 14:13:40 -0300

    first commit

---
+++ src/main/java/br/com/cleandev/whatsapp/WhatsappApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleandev.whatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappApplication.class, args);
+	}
+
+}
