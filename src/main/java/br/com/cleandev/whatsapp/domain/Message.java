0f69e308bc2a47da01adcdb2d247971417fbdeec
Danilo Cesar Mendes <danilocesarmendes@gmail.com>
2020-06-14 10:19:39 -0300

    adding annotation of lombok

--- src/main/java/br/com/cleandev/whatsapp/domain/Message.java
+++ src/main/java/br/com/cleandev/whatsapp/domain/Message.java
@@ -11,24 +11,36 @@ import javax.persistence.Table;
 
 import com.sun.istack.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+@ToString()
+@Builder
 @Table
 @Entity(name = "message")
-public class Message implements Serializable{
+public class Message implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotNull
 	private String menssage;
-	
+
 	@NotNull
 	private String sendPhone;
-	
+
 	@NotNull
 	private Date createdAt;
-	
+
 }
